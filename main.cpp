96f33da36229b9983273e26315aa51abd851beaa
Nilabhra Patra <nlbhrptr@hotmail.com>
2019-03-14 15:20:12 +0100

    Flow Vectors calculated

---
+++ main.cpp
@@ -0,0 +1,17 @@
+
+#include "FlowVector.h"
+
+int main(){
+	FlowVector f;
+	pair<int,int> obstacle = f.detected_obstacle();
+	//cout<<"left:" + obstacle.first<<endl;
+	//cout<<"right" + obstacle.second<<endl;
+}
+
+
+
+
+
+
+
+
